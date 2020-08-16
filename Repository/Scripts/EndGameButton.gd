91c840b95c977c0b1bde330f8d8a179fea2bfed1
Bonga Maswanganye <snowmehn@gmail.com>
2020-08-16 13:33:59 -0400

    the worlds quickest main menu
    i made this in 10 minutes dont flame me :(

---
+++ Repository/Scripts/EndGameButton.gd
@@ -0,0 +1,8 @@
+extends Button
+
+
+
+
+
+func _on_endgame_pressed():
+	get_tree().quit()
---
+++ Repository/Scripts/PracticeButton.gd
@@ -0,0 +1,6 @@
+
+extends Button
+
+
+func _on_Practice_pressed():
+	get_tree().change_scene("res://Node2D.tscn")
