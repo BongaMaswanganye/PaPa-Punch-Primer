c4d0b68cfc3ec55a5c0540c8d2673f376a82f8c1
jesim6207 <jesim6207@gmail.com>
2021-01-04 19:26:28 -0500

    Stuff changed for P2

--- Repository/Scripts/Player_2.gd
+++ Repository/Scripts/Player_2.gd
@@ -1,18 +1,39 @@
 extends KinematicBody2D
 
-
+export var Health = 80
+export var maxHealth = 100
 export var run_speed = 300
 export var jump_speed = -1100
 export var gravity = 2500
 
+
 var velocity = Vector2()
 var jumping = false
+var state_machine
+onready var ANI = get_node("AnimatedSprite")
+
+signal damaged2
+signal newHealth2
+signal newMaxHP2
+
+func _ready():
+	initialize()
+	state_machine = $AnimationTree.get("parameters/playback")
+	
 
 func get_input():
 	velocity.x = 0
+	
 	var right = Input.is_action_pressed("p2_right")
 	var left = Input.is_action_pressed("p2_left")
 	var jump = Input.is_action_just_pressed("p2_jump")
+#	var attack_1 = Input.is_action_just_pressed("p1_attack")
+	
+	var current_anim = state_machine.get_current_node()
+	
+	
+	if Input.is_action_just_pressed("damaged2"):
+		damaged()
 	
 	if jump and is_on_floor():
 		jumping = true
@@ -22,18 +43,48 @@ func get_input():
 	if left:
 		velocity.x -= run_speed
 
+
+#	if attack_1:
+#		$AnimatedSprite.play("Attack-1")
+	
+	if velocity.length()  == 0:
+		state_machine.travel("Idle")
+		
+	if velocity.length() > 0:
+		state_machine.travel("Walk")
+	
+
+func damaged():
+		Health -= 10
+		Health = clamp (Health, 0, maxHealth)
+		print("G was pressed, [NewPlayer.gd]")
+		print("Health: ", Health)
+		print("maxHealth: ", maxHealth)
+		emit_signal("damaged", Health)
+
+
 func _physics_process(delta):
 	get_input()
 	velocity.y += gravity * delta
+	
 	if jumping and is_on_floor():
 		jumping = false
 	velocity = move_and_slide(velocity, Vector2(0, -1))
 # Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
-#	pass
+
+#func _on_AnimatedSprite_animation_finished():
+#	$AnimatedSprite.play("Idle")
+
+func walking():
+	state_machine.travel("Walk")
+	
+
+func initialize():
+	Health = maxHealth
+	emit_signal("newHealth", Health)
+	emit_signal("newMaxHP", maxHealth)
 
